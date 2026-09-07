289663b63e0f3fe32792528502a6ce12d0626395
yonghwankim-dev <dragonbead95@naver.com>
2026-09-07 12:42:45 +0900

    test - add test

---
+++ 단위테스트/source/unit-test/src/test/java/com/nemo/unit_test/CalculatorTests.java
@@ -0,0 +1,25 @@
+package com.nemo.unit_test;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTests {
+	static class Calculator{
+
+		public double sum(double first, double second) {
+			return first + second;
+		}
+	}
+
+	@Test
+	void sum_of_two_numbers(){
+		// arrange
+		double first = 10;
+		double second = 20;
+		Calculator calculator = new Calculator();
+		// action
+		double result = calculator.sum(first, second);
+		// assert
+		Assertions.assertThat(result).isEqualTo(30);
+	}
+}
